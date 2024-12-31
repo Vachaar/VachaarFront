7c0e6ce83b79c18fdd6db6896b51a90c70a4ecf1
h.soleymani <hesamsoleymani20@gmail.com>
2025-01-01 02:37:51 +0330

    connects auth flow to backend

--- vachaar-next/app/login/page.tsx
+++ vachaar-next/app/login/page.tsx
@@ -1,21 +1,48 @@
 "use client";
 
 import { Input } from "@nextui-org/input";
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
   const router = useRouter();
 
-  const [isVisible, setIsVisible] = React.useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-  const toggleVisibility = () => setIsVisible(!isVisible);
+  const togglePasswordVisibility = () =>
+    setIsPasswordVisible(!isPasswordVisible);
 
   const handleLogin = () => {
-    router.push("/");
+    fetch("http://localhost/usr/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          toast.success("با موفقیت وارد شدید.");
+          router.push("/");
+        } else {
+          if (res.status === 429) {
+            toast.error(
+              "تعداد درخواست‌ها بیش از حد مجاز است. لطفاً بعداً تلاش کنید."
+            );
+            return;
+          }
+          toast.error("ایمیل یا رمز عبور اشتباه است.");
+        }
+      })
+      .catch(() => {
+        toast.error("خطا در ارتباط با سرور. لطفا اینترنت خود را بررسی کنید.");
+      });
   };
 
   return (
@@ -39,12 +66,14 @@ export default function LoginPage() {
             placeholder="ایمیل را وارد کنید"
             type="email"
             variant="bordered"
+            value={email}
+            onValueChange={setEmail}
           />
           <Input
             isRequired
             endContent={
-              <button type="button" onClick={toggleVisibility}>
-                {isVisible ? (
+              <button type="button" onClick={togglePasswordVisibility}>
+                {isPasswordVisible ? (
                   <Icon
                     className="pointer-events-none text-2xl text-default-400"
                     icon="solar:eye-closed-linear"
@@ -61,8 +90,10 @@ export default function LoginPage() {
             labelPlacement="outside"
             name="password"
             placeholder="رمز عبور را وارد کنید"
-            type={isVisible ? "text" : "password"}
+            type={isPasswordVisible ? "text" : "password"}
             variant="bordered"
+            value={password}
+            onValueChange={setPassword}
           />
           <Button color="primary" type="submit" onClick={handleLogin}>
             ورود
--- vachaar-next/app/register/page.tsx
+++ vachaar-next/app/register/page.tsx
@@ -1,80 +1,200 @@
 "use client";
 
 import { Input } from "@nextui-org/input";
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
+import toast from "react-hot-toast";
+import { InputOtp } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
-  const [isVisible, setIsVisible] = React.useState(false);
+  const router = useRouter();
 
-  const toggleVisibility = () => setIsVisible(!isVisible);
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [emailError, setEmailError] = useState<string | undefined>();
+  const [phoneError, setPhoneError] = useState<string | undefined>();
+  const [passwordError, setPasswordError] = useState<string | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCodeSent, setIsCodeSent] = useState(false);
+  const [code, setCode] = useState("");
+  const [codeError, setCodeError] = useState<string | undefined>();
+
+  const togglePasswordVisibility = () =>
+    setIsPasswordVisible(!isPasswordVisible);
+
+  const handleRegister = () => {
+    setIsLoading(true);
+    fetch("http://localhost/usr/register/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, phone, password }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          setIsCodeSent(true);
+          toast("لطفا کد ارسال شده به ایمیل را وارد کنید.");
+        } else {
+          if (res.status === 429) {
+            toast.error(
+              "تعداد درخواست‌ها بیش از حد مجاز است. لطفاً بعداً تلاش کنید."
+            );
+            return;
+          }
+          res.json().then((data) => {
+            setEmailError(data?.email?.[0]);
+            setPhoneError(data?.phone?.[0]);
+            setPasswordError(data?.password?.[0]);
+          });
+        }
+      })
+      .catch(() => {
+        toast.error("خطا در ارتباط با سرور. لطفا اینترنت خود را بررسی کنید.");
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleVerifyEmail = () => {
+    fetch("http://localhost/usr/verify-email/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, code }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          toast.success("ثبت نام شما با موفقیت انجام شد. لطفا وارد شوید.");
+          router.push("/login");
+        } else {
+          if (res.status === 429) {
+            toast.error(
+              "تعداد درخواست‌ها بیش از حد مجاز است. لطفاً بعداً تلاش کنید."
+            );
+            return;
+          }
+          setCodeError("کد وارد شده صحیح نیست");
+        }
+      })
+      .catch(() => {
+        toast.error("خطا در ارتباط با سرور. لطفا اینترنت خود را بررسی کنید.");
+      });
+  };
 
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6">
-        <p className="pb-4 text-3xl font-semibold">
-          ثبت نام
-          <span aria-label="emoji" className="ml-2" role="img">
-            👋
-          </span>
-        </p>
-        <form
-          className="flex flex-col gap-4"
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <Input
-            isRequired
-            label="نام کاربری"
-            labelPlacement="outside"
-            name="username"
-            placeholder="نام کاربری را وارد کنید"
-            type="text"
-            variant="bordered"
-          />
-          <Input
-            isRequired
-            label="ایمیل"
-            labelPlacement="outside"
-            name="email"
-            placeholder="ایمیل را وارد کنید"
-            type="email"
-            variant="bordered"
-          />
-          <Input
-            isRequired
-            endContent={
-              <button type="button" onClick={toggleVisibility}>
-                {isVisible ? (
-                  <Icon
-                    className="pointer-events-none text-2xl text-default-400"
-                    icon="solar:eye-closed-linear"
-                  />
-                ) : (
-                  <Icon
-                    className="pointer-events-none text-2xl text-default-400"
-                    icon="solar:eye-bold"
-                  />
-                )}
-              </button>
-            }
-            label="رمز عبور"
-            labelPlacement="outside"
-            name="password"
-            placeholder="رمز عبور را وارد کنید"
-            type={isVisible ? "text" : "password"}
-            variant="bordered"
-          />
-          <Button color="primary" type="submit">
-            ثبت نام
-          </Button>
-        </form>
-        <p className="text-center text-small">
-          <Link href="/login" size="sm">
-            حساب کاربری دارید؟ وارد شوید
-          </Link>
-        </p>
+        {isCodeSent ? (
+          <div className="flex flex-col items-center gap-2" dir={"ltr"}>
+            <label
+              htmlFor="file-input"
+              className="block text-sm text-foreground"
+            >
+              کد ارسال شده به ایمیل را وارد کنید
+            </label>
+            <InputOtp
+              length={6}
+              value={code}
+              onValueChange={setCode}
+              onChange={() => setCodeError(undefined)}
+              variant="bordered"
+              onComplete={handleVerifyEmail}
+              errorMessage={codeError}
+              isInvalid={codeError ? true : undefined}
+              className={"text-center"}
+            />
+          </div>
+        ) : (
+          <>
+            <p className="pb-4 text-3xl font-semibold">
+              ثبت نام
+              <span aria-label="emoji" className="ml-2" role="img">
+                👋
+              </span>
+            </p>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={(e) => e.preventDefault()}
+            >
+              <Input
+                isRequired
+                label="ایمیل"
+                labelPlacement="outside"
+                name="email"
+                placeholder="ایمیل را وارد کنید"
+                type="email"
+                variant="bordered"
+                value={email}
+                onValueChange={setEmail}
+                onChange={() => setEmailError(undefined)}
+                errorMessage={emailError}
+                isInvalid={emailError ? true : undefined}
+              />
+              <Input
+                isRequired
+                label="شماره موبایل"
+                labelPlacement="outside"
+                name="phone"
+                placeholder="شماره موبایل خود را وارد کنید."
+                type="number"
+                variant="bordered"
+                value={phone}
+                onValueChange={setPhone}
+                onChange={() => setPhoneError(undefined)}
+                errorMessage={phoneError}
+                isInvalid={phoneError ? true : undefined}
+              />
+              <Input
+                isRequired
+                endContent={
+                  <button type="button" onClick={togglePasswordVisibility}>
+                    {isPasswordVisible ? (
+                      <Icon
+                        className="pointer-events-none text-2xl text-default-400"
+                        icon="solar:eye-closed-linear"
+                      />
+                    ) : (
+                      <Icon
+                        className="pointer-events-none text-2xl text-default-400"
+                        icon="solar:eye-bold"
+                      />
+                    )}
+                  </button>
+                }
+                label="رمز عبور"
+                labelPlacement="outside"
+                name="password"
+                placeholder="رمز عبور را وارد کنید"
+                type={isPasswordVisible ? "text" : "password"}
+                variant="bordered"
+                value={password}
+                onValueChange={setPassword}
+                onChange={() => setPasswordError(undefined)}
+                errorMessage={passwordError}
+                isInvalid={passwordError ? true : undefined}
+              />
+              <Button
+                color="primary"
+                type="submit"
+                onClick={handleRegister}
+                isLoading={isLoading}
+              >
+                ثبت نام
+              </Button>
+            </form>
+            <p className="text-center text-small">
+              <Link href="/login" size="sm">
+                حساب کاربری دارید؟ وارد شوید
+              </Link>
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
