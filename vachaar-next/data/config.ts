b02873df78e23357e1a1b7b9972b4afbeccee990
h.soleymani <hesamsoleymani20@gmail.com>
2025-01-07 22:02:18 +0330

    adds report reasons config

---
+++ vachaar-next/data/config.ts
@@ -0,0 +1,48 @@
+export const categories = [
+  { title: "املاک", id: 1, icon: "home-bold" },
+  { title: "وسایل نقلیه", id: 2, icon: "wheel-bold" },
+  { title: "کالای دیجیتال", id: 3, icon: "devices-bold" },
+  { title: "خانه و آشپزخانه", id: 4, icon: "tea-cup-bold" },
+  { title: "وسایل شخصی", id: 5, icon: "user-bold" },
+  { title: "سرگرمی", id: 6, icon: "gamepad-bold" },
+  { title: "سایر", id: 7, icon: "menu-dots-bold" },
+];
+
+export const ReportReasons = [
+  {
+    title: "کلاهبرداری",
+    id: 1,
+  },
+  {
+    title: "غیرقانونی",
+    id: 2,
+  },
+  {
+    title: "غیراخلاقی",
+    id: 3,
+  },
+  {
+    title: "قیمت نادرست",
+    id: 4,
+  },
+  {
+    title: "اطلاعات تماس نادرست",
+    id: 5,
+  },
+  {
+    title: "دسته‌بندی نادرست",
+    id: 6,
+  },
+  {
+    title: "ناموجودی یا پاسخگو نبودن",
+    id: 7,
+  },
+  {
+    title: "تکراری یا اسپم",
+    id: 8,
+  },
+  {
+    title: "سایر دلایل",
+    id: 9,
+  },
+];
--- vachaar-next/data/mock-data.ts
+++
@@ -1,9 +0,0 @@
-export const categories = [
-  { title: "املاک", id: 1, icon: "home-bold" },
-  { title: "وسایل نقلیه", id: 2, icon: "wheel-bold" },
-  { title: "کالای دیجیتال", id: 3, icon: "devices-bold" },
-  { title: "خانه و آشپزخانه", id: 4, icon: "tea-cup-bold" },
-  { title: "وسایل شخصی", id: 5, icon: "user-bold" },
-  { title: "سرگرمی", id: 6, icon: "gamepad-bold" },
-  { title: "سایر", id: 7, icon: "menu-dots-bold" },
-];
